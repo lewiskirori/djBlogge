9e33442a498106c55c4fd214281d44fbe5e70d22
lewiskirori <amicableycot@gmail.com>
2023-05-16 13:29:19 +0300

    b post feed

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import BlogPost
+
+def create_blog_view(request):
+    return render(request, "blog/create_blog.htm", {})
