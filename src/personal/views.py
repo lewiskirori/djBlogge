f4557ee34f522d36089ae5cf490b9d7daa45827f
lewiskirori <amicableycot@gmail.com>
2023-06-07 20:54:07 +0300

    top-right-snippet

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from datetime import datetime
-from account.models import Account
+from operator import attrgetter
+from blog.models import BlogPost
 
 def home_screen_view(request):
     context = {}
-
-    accounts = Account.objects.all()
-    context['accounts'] = accounts
+    blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
+    context['blog_posts'] = blog_posts
 
     current_year = datetime.now().year
     context['current_year'] = current_year
