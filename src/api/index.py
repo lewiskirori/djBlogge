e105e58d6d31239ca686f8a2cc75b267e97dce7c
lewiskirori <amicableycot@gmail.com>
2023-08-08 17:38:39 +0300

    settings structure

--- src/api/index.py
+++
@@ -1,9 +0,0 @@
-from django.core.wsgi import get_wsgi_application
-from vercel_serverless_sdk import VercelServerlessSdk
-
-sdk = VercelServerlessSdk()
-
-def handler(request):
-    application = get_wsgi_application()
-    response = sdk.WsgiHandler(application, request)
-    return response
