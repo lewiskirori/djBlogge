7039d928766be5928f3de8d330196e08101ccb07
lewiskirori <amicableycot@gmail.com>
2023-08-25 19:40:48 +0300

    application package

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['djblogge.com', 'djblogge.up.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['djblogge.up.railway.app', '127.0.0.1', 'localhost']
 
 if DEBUG:
     #During development only
