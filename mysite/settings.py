0ecaf025b7948e112eb3a06916c4618e47b406d8
lewiskirori <amicableycot@gmail.com>
2023-08-28 18:28:54 +0300

    application package

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['djblogge.up.railway.app', 'djblogge.railway.internal', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [os.environ.get('TRUSTED_ORIGIN', '127.0.0.1'), 'localhost']
 
 if DEBUG:
     #During development only
