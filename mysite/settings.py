490de28415b5404a6b2381cf634bbaada037c97c
lewiskirori <amicableycot@gmail.com>
2023-08-30 01:27:16 +0300

    app package

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media_cdn/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
