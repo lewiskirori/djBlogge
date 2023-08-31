aa5bed2b2956628d4ee212a8264a7f139e845e26
lewiskirori <amicableycot@gmail.com>
2023-08-31 21:38:00 +0300

    app package

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
