77e340b84fc03792d75817f3a6642806eb45c803
lewiskirori <amicableycot@gmail.com>
2023-09-01 22:05:58 +0300

    app package

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,8 +9,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
