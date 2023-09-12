4d7e052a5f047f5d391e325ac755d282a2bed45b
lewiskirori <amicableycot@gmail.com>
2023-09-12 21:08:26 +0300

    application bundle

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['djblogge.up.railway.app', '127.0.0.1', 'localhost']
 
