5d4d205c1c98837d1e25bda3bfa5dd415306f49c
lewiskirori <amicableycot@gmail.com>
2023-09-20 09:28:37 +0300

    app bundle

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,8 +104,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
