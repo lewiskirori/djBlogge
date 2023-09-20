69eae4f5e8438c27bff211de6d9c50f820acce42
lewiskirori <amicableycot@gmail.com>
2023-09-20 10:00:31 +0300

    app bundle

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR / 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
