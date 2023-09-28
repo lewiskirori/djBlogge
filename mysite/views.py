4ae048d205076b54ad47e6420a4a63b369b7f42e
lewiskirori <amicableycot@gmail.com>
2023-09-28 10:31:46 +0300

    env info config

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def custom_404(request, exception):
-    return render(request, '404.htm', status=404)
+    return render(request, '404/404.htm', status=404)
 
 def custom_500(request):
-    return render(request, '500.htm', status=500)
+    return render(request, '500/500.htm', status=500)
