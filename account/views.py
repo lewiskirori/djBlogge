14604ae13fb11a1d1690f8c6cd227b796bfb3ef1
lewiskirori <amicableycot@gmail.com>
2023-10-04 20:07:13 +0300

    background usability

--- account/views.py
+++ account/views.py
@@ -1,10 +1,6 @@
 from datetime import datetime
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.tokens import default_token_generator
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
 from account.forms import RegistrationForm, AccountAuthenticationForm, AccountUpdateForm
 from blog.models import BlogPost
 
@@ -100,17 +96,3 @@ def account_view(request):
 
 def must_authenticate_view(request):
     return render(request, 'account/must_authenticate.htm', {})
-
-def password_reset_request(request):
-    if request.method == "POST":
-
-        uid = urlsafe_base64_encode(force_bytes(user.pk))
-        token = default_token_generator.make_token(user)
-
-        current_site = get_current_site(request)
-        protocol = 'https' if request.is_secure() else 'http'
-        domain = current_site.domain
-
-        reset_url = f"{protocol}://{domain}{reverse('password_reset_confirm', kwargs={'uidb64': uid, 'token': token})}"
-
-        send_password_reset_email(user.email, reset_url) 
