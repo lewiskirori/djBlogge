8e417ac99f22ee5f464442a52e58818f859e61b9
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2024-02-10 10:07:59 +0300

    provider's specs

--- mysite/settings.py
+++ mysite/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['djblogge.up.railway.app', '127.0.0.1', 'localhost', '*']
 
